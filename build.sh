701ca67b5a4a753c4508bd0cf9ee2746e442084b
James Barnsley <james@barnsley.nz>
2017-01-13 08:23:07 +1300

    Fixing issues where cachebuster wasn't working

--- build.sh
+++ build.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
 if [ ! -d public_html ]; then
+	echo -e Creating mopidy_iris/static folder
 	ln -s mopidy_iris/static public_html
 fi
 
 # copy assets
+echo -e Copying assets $SERVER
 cp src/.htaccess mopidy_iris/static/.htaccess
-rsync -avr src/assets/ mopidy_iris/static/assets/
+rsync -avrs src/assets/ mopidy_iris/static/assets/ 2>&1 >/dev/null
 
 # create HTML files
+echo -e Copying index.html and test.html
 cp src/index.html mopidy_iris/static/index.html
 cp src/index.html mopidy_iris/static/test.html
 
 # process production html
-CACHEBUSTER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+CACHEBUSTER=$(date | md5sum | cut -f1 -d' ')
+echo -e Cachebusting js/css URLs $CACHEBUSTER
 sed -i 's/app.js/app.min.js?'$CACHEBUSTER'/g' mopidy_iris/static/index.html
 sed -i 's/app.css/app.min.css?'$CACHEBUSTER'/g' mopidy_iris/static/index.html
+
+echo -e "\x1b[32;01m"Done!"\x1b[39;49;00m"
