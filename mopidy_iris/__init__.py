747709fac8b96a189c1e35c90c310fe4b014ccd8
James Barnsley <james@barnsley.nz>
2017-01-19 16:15:25 +1300

    Major release

--- mopidy_iris/__init__.py
+++ mopidy_iris/__init__.py
@@ -8,7 +8,7 @@ from frontend import IrisFrontend
 from http import RequestHandler
 
 logger = logging.getLogger(__name__)
-__version__ = '2.10.17'
+__version__ = '2.11.0'
 
 ##
 # Core extension class
