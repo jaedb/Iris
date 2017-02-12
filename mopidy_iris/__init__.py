e61831f029bcb956d61285239005be860b3d2567
James Barnsley <james@barnsley.nz>
2017-02-13 08:10:57 +1300

    Version promotion; Buttons on responsive

--- mopidy_iris/__init__.py
+++ mopidy_iris/__init__.py
@@ -8,7 +8,7 @@ from frontend import IrisFrontend
 from http import RequestHandler
 
 logger = logging.getLogger(__name__)
-__version__ = '2.11.4'
+__version__ = '2.11.5'
 
 ##
 # Core extension class
