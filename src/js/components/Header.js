0a2f0f2a0c9f9d21a300afc48daa9944be0df228
James Barnsley <james@barnsley.nz>
2017-02-22 08:36:17 +1300

    Condense actions into expansion panel

--- src/js/components/Header.js
+++ src/js/components/Header.js
@@ -1,5 +1,7 @@
 
 import React, { PropTypes } from 'react'
+import FontAwesome from 'react-fontawesome'
+
 import Icon from './Icon'
 import SidebarToggleButton from './SidebarToggleButton'
 
@@ -7,6 +9,27 @@ export default class Header extends React.Component{
 
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			expanded: false
+		}
+	}
+
+	renderActions(){
+		if (!this.props.actions) return null
+
+		return (
+			<div className={this.state.expanded ? 'expanded actions' : 'actions'}>
+				<button className="trigger" onClick={e => this.setState({expanded: !this.state.expanded})}>
+					<FontAwesome name="ellipsis-v" />
+				</button>
+				<span className="items">
+					<span className="liner">
+						{ this.props.actions }
+					</span>
+				</span>
+			</div>
+		)
 	}
 
 	render(){
@@ -15,7 +38,7 @@ export default class Header extends React.Component{
 				<Icon name={ this.props.icon } />
 				<SidebarToggleButton />
 				<h1>{ this.props.title }</h1>
-				{ this.props.actions ? <div className="actions">{ this.props.actions }</div> : null }
+				{this.renderActions()}
 			</header>
 		);
 	}
