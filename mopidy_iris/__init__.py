30b5b1674d5476770d01ebadbc53a032edc5bb22
James Barnsley <james@barnsley.nz>
2017-05-16 13:06:26 +1200

    2.14.0

--- mopidy_iris/__init__.py
+++ mopidy_iris/__init__.py
@@ -12,7 +12,7 @@ from handlers import WebsocketHandler, HttpHandler
 from core import IrisCore
 
 logger = logging.getLogger(__name__)
-__version__ = '2.13.17'
+__version__ = '2.14.0'
 
 ##
 # Core extension class
