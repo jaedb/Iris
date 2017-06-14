ffe790a33b47017a84c1e34d6fd639527ed89386
James Barnsley <james@barnsley.nz>
2017-06-14 15:58:13 +1200

    Version promotion 2.14.5

--- mopidy_iris/__init__.py
+++ mopidy_iris/__init__.py
@@ -12,7 +12,7 @@ from handlers import WebsocketHandler, HttpHandler
 from core import IrisCore
 
 logger = logging.getLogger(__name__)
-__version__ = '2.14.4'
+__version__ = '2.14.5'
 
 ##
 # Core extension class
