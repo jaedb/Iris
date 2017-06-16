0fa7f4bca8fd90beca198f2589d7bd86444984ed
James Barnsley <james@barnsley.nz>
2017-06-17 08:37:01 +1200

    White icons

--- src/js/components/Sidebar.js
+++ src/js/components/Sidebar.js
@@ -32,7 +32,7 @@ class Sidebar extends React.Component{
 
 		        		<section>
 							<Link activeClassName="active" to={global.baseURL+"queue"}>
-								<Icon name="play" />
+								<Icon className="white" name="play" />
 								Now playing
 							</Link>
 						</section>
@@ -40,19 +40,19 @@ class Sidebar extends React.Component{
 						<section>
 							<title>Discover</title>
 							<Link activeClassName="active" to={global.baseURL+"discover/recommendations"}>
-								<Icon name="compass" />
+								<Icon className="white" name="compass" />
 								Discover
 							</Link>
 							<Link activeClassName="active" to={global.baseURL+"discover/categories"}>
-								<Icon name="grid" />
+								<Icon className="white" name="grid" />
 								Genre / Mood
 							</Link>
 							<Link activeClassName="active" to={global.baseURL+"discover/featured"}>
-								<Icon name="star" />
+								<Icon className="white" name="star" />
 								Featured playlists
 							</Link>
 							<Link activeClassName="active" to={global.baseURL+"discover/new-releases"}>
-								<Icon name="leaf" />
+								<Icon className="white" name="leaf" />
 								New releases
 							</Link>
 						</section>
@@ -60,30 +60,30 @@ class Sidebar extends React.Component{
 						<section>
 							<title>My Music</title>
 							<Link activeClassName="active" to={global.baseURL+"library/playlists"}>
-								<Icon name="playlist" />
+								<Icon className="white" name="playlist" />
 								Playlists
 							</Link>
 							<Link activeClassName="active" disabled={!this.props.spotify_authorized} to={this.props.spotify_authorized ? global.baseURL+"library/artists" : null}>
-								<Icon name="mic" />
+								<Icon className="white" name="mic" />
 								Artists
 							</Link>
 							<Link activeClassName="active" disabled={!this.props.spotify_authorized} to={this.props.spotify_authorized ? global.baseURL+"library/albums" : null}>
-								<Icon name="cd" />
+								<Icon className="white" name="cd" />
 								Albums
 							</Link>
 							<Link activeClassName="active" disabled={!this.props.spotify_authorized} to={this.props.spotify_authorized ? global.baseURL+"library/tracks" : null}>
-								<Icon name="music" />
+								<Icon className="white" name="music" />
 								Tracks
 							</Link>
 							<Link activeClassName="active" to={global.baseURL+"library/local"}>
-								<Icon name="folder" />
+								<Icon className="white" name="folder" />
 								Local
 							</Link>
 						</section>
 
 						<section>
 							<Link activeClassName="active" to={global.baseURL+"settings"}>
-								<Icon name="cog" />
+								<Icon className="white" name="cog" />
 								Settings
 								{ !this.props.mopidy_connected || !this.props.spotify_connected || !this.props.pusher_connected ? <FontAwesome name="exclamation-triangle" className="red-text pull-right" /> : null }
 							</Link>
