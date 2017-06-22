278d6f73be368f8b1060ab456c748ad4eb9b67c7
James Barnsley <james@barnsley.nz>
2017-06-23 08:03:28 +1200

    Colouring for player; Context menu and track tap zone

--- src/js/components/Track.js
+++ src/js/components/Track.js
@@ -140,8 +140,7 @@ export default class Track extends React.Component{
 				onDoubleClick={ e => this.props.handleDoubleClick(e) }
 				onContextMenu={ e => this.handleContextMenu(e) }>
 					<span className="state-icon">
-						{ this.props.track.selected ? <FontAwesome name="check" className="selected" fixedWidth /> : null }
-						{ this.props.track.playing && !this.props.track.selected ? <FontAwesome name="play" className="playing" fixedWidth /> : null }
+						{this.props.track.selected ? <FontAwesome name="check" className="selected" fixedWidth /> : null}
 					</span>
 					{ track_columns }
 			</div>
