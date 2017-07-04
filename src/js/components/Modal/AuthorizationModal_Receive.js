414172858285c096a6f2189e8ba4ed4054c17036
James Barnsley <james@barnsley.nz>
2017-07-05 08:00:57 +1200

    Authorization import modal styles

--- src/js/components/Modal/AuthorizationModal_Receive.js
+++ src/js/components/Modal/AuthorizationModal_Receive.js
@@ -33,20 +33,18 @@ export default class extends React.Component{
 				<h1>Spotify authorization received</h1>
 				<h2 className="grey-text">{user.display_name ? user.display_name : user.id} has shared their Spotify authorization with you. Importing this will overwrite your existing Spotify Authorization.</h2>
 				
-				<div className="user-view">
-					<div className="intro">
-						<Thumbnail images={user.images} circle />
-						<h1>{user.display_name ? user.display_name : user.id}</h1>
-						<ul className="details">
-							<li>{user.id}</li>
-							<li>{user.followers.total} followers</li>
-						</ul>
-					</div>
+				<div className="user-details">
+					<Thumbnail images={user.images} circle />
+					<h2>{user.display_name ? user.display_name : user.id}</h2>
+					<ul className="details">
+						<li>{user.id}</li>
+						<li>{user.followers.total} followers</li>
+					</ul>
 				</div>
 
 				<div className="actions centered-text">
 					<button onClick={e => this.props.uiActions.closeModal()}>Ignore</button>
-					<button className="primary" onClick={e => this.handleImport(e)}>Import</button>
+					<button className="primary" onClick={e => this.handleImport(e)}>Import authorization</button>
 				</div>
 			</div>
 		)
