ccd5064a534e6355ff6b1d41f57898a214afc2b0
andrzej <araczkowski@gmail.com>
2017-08-21 14:10:56 +0200

    add ais to sidebar menu

--- src/js/components/Sidebar.js
+++ src/js/components/Sidebar.js
@@ -62,6 +62,36 @@ class Sidebar extends React.Component{
 							</Link>
 						</section> : null}
 
+						{this.props.ais_enabled &&
+						<section>
+							<title>AI-Speaker</title>
+							<Link className={this.linkClassName('library/browse/ais:root:1')} to={global.baseURL+"library/browse/ais:root:1"}>
+								<Icon name="ais" />
+								Discover
+							</Link>
+							<Link className={this.linkClassName('library/playlists')} to={global.baseURL+"library/playlists"}>
+								<Icon name="playlist" />
+								Playlists
+							</Link>
+							<Link className={this.linkClassName('library/browse/ais:root:2')} to={global.baseURL+"library/browse/ais:root:2"}>
+								<Icon name="ais-library" />
+								My Library
+							</Link>
+							<Link className={this.linkClassName('library/browse/ais:root:3')} to={global.baseURL+"library/browse/ais:root:3"}>
+								<Icon name="ais-family" />
+								My Family
+							</Link>
+							<Link className={this.linkClassName('library/browse/ais:root:4')} to={global.baseURL+"library/browse/ais:root:4"}>
+								<Icon name="ais-likes" />
+								My Likes
+							</Link>
+							<Link className={this.linkClassName('library/browse/ais:root:5')} to={global.baseURL+"library/browse/ais:root:5"}>
+								<Icon name="ais-pendrive" />
+								Pendrive
+							</Link>
+						</section>}
+
+						{!this.props.ais_enabled &&
 						<section>
 							<title>My Music</title>
 							<Link className={this.linkClassName('library/playlists')} to={global.baseURL+"library/playlists"}>
@@ -84,7 +114,7 @@ class Sidebar extends React.Component{
 								<Icon name="folder" />
 								Browse
 							</Link>
-						</section>
+						</section>}
 
 						<section>
 							<Link className={this.linkClassName('settings')} to={global.baseURL+"settings"}>
@@ -118,7 +148,8 @@ const mapStateToProps = (state, ownProps) => {
 		spotify_enabled: state.spotify.enabled,
 		spotify_connected: state.spotify.connected,
 		spotify_authorized: state.spotify.authorization,
-		dragger: state.ui.dragger
+		dragger: state.ui.dragger,
+		ais_enabled: state.mopidy.uri_schemes.indexOf('ais:') > -1
 	}
 }
 
