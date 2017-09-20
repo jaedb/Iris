200bfeddc458b731f1f7d8b6d19d33290a0312a6
James Barnsley <james@barnsley.nz>
2017-09-21 09:27:58 +1200

    Pushing tag directly

--- release.sh
+++ release.sh
@@ -16,8 +16,8 @@ echo -e "git tag -a '$VERSION' -m 'Releasing $VERSION'"
 TAG=$(git tag -a "$VERSION" -m "Releasing $VERSION")
 echo -e $TAG
 
-echo -e "git push"
-PUSH=$(git push)
+echo -e "git push origin $VERSION"
+PUSH=$(git push origin "$VERSION")
 echo -e $PUSH
 
 echo -e "\x1b[32;01m"Done!"\x1b[39;49;00m"
