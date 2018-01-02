93eb1daee410e35ef654ef365423ffb6122db5f4
James Barnsley <james@barnsley.nz>
2018-01-02 15:58:55 +1300

    Merging SearchResult.Artist and SearchResult.Album arrays in favor of digested SearchResult.Track assumptions, fixes #244

--- src/js/components/ArtistGrid.js
+++ src/js/components/ArtistGrid.js
@@ -48,9 +48,9 @@ class ArtistGrid extends React.Component{
 											<Thumbnail size="medium" images={artist.images} />
 											<div className="name">
 												{artist.name}
-												{this.props.show_source_icon ? <FontAwesome name={helpers.sourceIcon(artist.uri)} className="source" fixedWidth /> : null}
 											</div>
 											<div className="secondary">
+												{this.props.show_source_icon ? <FontAwesome name={helpers.sourceIcon(artist.uri)} className="source" /> : null}
 												{artist.followers ? artist.followers.total.toLocaleString()+' followers' : null}
 												{artist.albums_uris && !artist.followers ? artist.albums_uris.length+' albums' : null}
 											</div>
