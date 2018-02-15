85ab348fa34426460cf1d4ea5f00de4a02b585aa
James Barnsley <james@barnsley.nz>
2018-02-16 07:46:02 +1300

    Blank icon for loading

--- src/js/components/Icon.js
+++ src/js/components/Icon.js
@@ -7,7 +7,7 @@ export default class Icon extends React.Component{
 		super(props);
 
 		this.state = {
-			data: null
+			data: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 16 16" style="enable-background:new 0 0 16 16;" xml:space="preserve"></svg>'
 		}
 	}
 
