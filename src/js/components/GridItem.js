e40af6185b9cd6d76691b987563bc80fd92061e7
James Barnsley <james@barnsley.nz>
2018-04-10 21:02:19 +1200

    Grid type as a classname for tailoring thumbnails

--- src/js/components/GridItem.js
+++ src/js/components/GridItem.js
@@ -98,7 +98,7 @@ export default class GridItem extends React.Component{
 		}
 
 		return (
-			<div className="grid-item" onClick={e => this.handleClick(e)} onContextMenu={e => this.handleContextMenu(e)}>
+			<div className={"grid-item "+this.props.type+"-grid-item"} onClick={e => this.handleClick(e)} onContextMenu={e => this.handleContextMenu(e)}>
 				<Thumbnail size="medium" images={images} />
 				<div className="name">
 					{item.name ? item.name : <span className="dark-grey-text">{item.uri}</span>}
