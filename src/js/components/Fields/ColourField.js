f2cb39ba97ce709963c983d20c562990a68d3ead
James Barnsley <james@barnsley.nz>
2018-10-13 23:13:43 +1300

    Icon and Colour fields for managing commands

--- src/js/components/Fields/ColourField.js
+++ src/js/components/Fields/ColourField.js
@@ -12,6 +12,7 @@ export default class ColourField extends React.Component{
 
 		var colours = [
 			'',
+			'white',
 			'mid_grey',
 			'grey',
 			'dark_grey',
@@ -29,12 +30,22 @@ export default class ColourField extends React.Component{
 			<div className="colour-field">
 				{
 					colours.map(colour => {
+						switch (colour){
+							case 'yellow':
+							case 'white':
+							case 'light_blue':
+								var text_colour = "black";
+								break;
+							default:
+								var text_colour = "white";
+								break;
+						}
 						return (
 							<div 
 								key={colour}
 								className={"colour-field__option "+(colour ? colour+"-background " : "")+(this.props.colour == colour ? "colour-field__option--selected" : "")}
 								onClick={e => this.props.onChange(colour)}>
-								{this.props.colour == colour ? <Icon name="check" className="colour-field__option__icon" /> : null}
+								{this.props.colour == colour ? <Icon name="check" className={"colour-field__option__icon "+text_colour+"-text"} /> : null}
 							</div>
 						);
 					})
