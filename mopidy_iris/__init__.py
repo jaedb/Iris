5dfd4c57174d17ae3d69bef0544c1bbadd7187b7
James Barnsley <james@barnsley.nz>
2018-12-21 10:12:39 +1300

    Merge and buildout

--- mopidy_iris/__init__.py
+++ mopidy_iris/__init__.py
@@ -12,7 +12,7 @@ from handlers import WebsocketHandler, HttpHandler
 from core import IrisCore
 
 logger = logging.getLogger(__name__)
-__version__ = '3.31.6'
+__version__ = '3.31.7'
 
 ##
 # Core extension class
