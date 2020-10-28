6da4181fa744b4ba5ecbc331764cb510937006ea
James Barnsley <james@barnsley.nz>
2020-10-28 21:24:38 +1300

    Updating tests - state seems broken, needs attention

--- __tests__/views/Album.test.js
+++ __tests__/views/Album.test.js
@@ -1,33 +1,39 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 
-// Testing-specific
-import { shallow, mount, render } from 'enzyme';
-const state = require('../state');
-
-// Test subjects
 import { Album } from '../../src/js/views/Album';
 import * as uiActions from '../../src/js/services/ui/actions';
 import * as coreActions from '../../src/js/services/core/actions';
+const state = require('../state');
 
 describe('<Album />', () => {
+  const album = {
+    uri: 'jest:album:one',
+    name: 'One',
+    artists_uris: [
+      'jest:artist:alpha',
+    ],
+    tracks_uris: [
+      'jest:track:one',
+      'jest:track:two',
+    ],
+    wiki: 'Wiki text',
+  };
 
-	var album = state.core.albums['jest:album:one'];
-
-	it('should render accurately', () => {
-		const dom = shallow(
-			<Album 
+  it('should render accurately', () => {
+    const dom = shallow(
+			<Album
 				album={album}
 				uiActions={uiActions}
 				coreActions={coreActions}
 			/>,
 			{
-				disableLifecycleMethods: true,
+			  disableLifecycleMethods: true,
 			},
-		);
+    );
 
-		expect(dom.find('.album-view').length).toBe(1);
-
-		expect(dom.find('h1').text()).toEqual('One');
-		expect(dom.find('.wiki__text p').text()).toEqual('Wiki text');
-	});
+    expect(dom.find('.album-view').length).toBe(1);
+    expect(dom.find('h1').text()).toEqual('One');
+    expect(dom.find('.wiki__text p').text()).toEqual('Wiki text');
+  });
 });
