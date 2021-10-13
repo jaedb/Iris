a2c6674a26b128081c1eb8a1e039a62f4920d531
James Barnsley <james@barnsley.nz>
2021-10-13 21:10:59 +1300

    Proof-of-concept outputs panel upgrade, #690

--- src/js/components/Fields/OutputControl.js
+++ src/js/components/Fields/OutputControl.js
@@ -1,16 +1,167 @@
 import React, { useState, useEffect } from 'react';
 import { connect, useSelector, useDispatch } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { find, groupBy, map } from 'lodash';
 import VolumeControl from './VolumeControl';
 import MuteControl from './MuteControl';
 import Icon from '../Icon';
 import DropdownField from './DropdownField';
+import Thumbnail from '../Thumbnail';
+import LinksSentence from '../LinksSentence';
 import * as coreActions from '../../services/core/actions';
+import * as mopidyActions from '../../services/mopidy/actions';
 import * as pusherActions from '../../services/pusher/actions';
 import * as snapcastActions from '../../services/snapcast/actions';
 import { sortItems, indexToArray, applyFilter } from '../../util/arrays';
 import { collate } from '../../util/format';
+import { titleCase } from '../../util/helpers';
 import { I18n } from '../../locale';
+
+const Header = ({ stream, server }) => {
+  const dispatch = useDispatch();
+  if (server) {
+    const {
+      id,
+      name,
+      current_track,
+      playback_state,
+    } = server;
+    const current_server = useSelector((state) => state.mopidy.current_server);
+    let stateIcon = '';
+    switch (playback_state) {
+      case 'playing':
+        stateIcon = 'play_arrow';
+        break;
+      case 'paused':
+        stateIcon = 'pause';
+        break;
+      default:
+        stateIcon = 'stop';
+        break;
+    }
+    return (
+      <div className="output-control__output__header">
+        <Thumbnail
+          images={current_track?.images}
+          size="small"
+          className="output-control__output__header__thumbnail"
+        />
+        <div className="output-control__output__header__content">
+          <h5 className="output-control__output__header__title">
+            {name}
+            <Icon name={stateIcon} />
+            {id !== current_server && (
+              <span
+                className="flag flag--default"
+                onClick={() => dispatch(mopidyActions.setCurrentServer(server))}
+              >
+                SWITCH TO THIS SERVER
+              </span>
+            )}
+          </h5>
+          {current_track ? (
+            <ul className="details">
+              <li>{current_track?.name}</li>
+              <li><LinksSentence items={current_track?.artists} type="artist" nolinks /></li>
+            </ul>
+          ) : (
+            <div className="details">
+              <I18n path="modal.servers.nothing_playing" />
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="output-control__output__header">
+      No server: {stream?.id}
+    </div>
+  );
+}
+
+const Clients = ({ clients }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="output-control__output__clients">
+      {clients.map((client) => {
+        const {
+          id,
+          name,
+          mute,
+          volume,
+        } = client;
+        return (
+          <div className="output-control__output__clients__item" key={client.id}>
+            <h5 className="output-control__output__clients__item__title">
+              {titleCase(name)}
+            </h5>
+            <div className="output-control__output__clients__item__volume">
+              <MuteControl
+                noTooltip
+                mute={mute}
+                onMuteChange={(value) => dispatch(snapcastActions.setClientMute(id, value))}
+              />
+              <VolumeControl
+                volume={volume}
+                mute={mute}
+                onVolumeChange={
+                  (value) => dispatch(snapcastActions.setClientVolume(id, value))
+                }
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+const Group = ({
+  item: {
+    stream,
+    server,
+    clients,
+  },
+}) => {
+  return (
+    <div className="output-control__output">
+      <Header stream={stream} server={server} />
+      <Clients clients={clients} />
+    </div>
+  );
+}
+
+const Outputs = () => {
+  const dispatch = useDispatch();
+  const groups = indexToArray(useSelector((state) => state.snapcast.groups || {}));
+  const clients = useSelector((state) => state.snapcast.clients || {});
+  const streams = useSelector((state) => state.snapcast.streams || {});
+  const servers = indexToArray(useSelector((state) => state.mopidy.servers || {}));
+  const groupsByStream = groupBy(groups, 'stream_id');
+
+  useEffect(() => {
+    servers.forEach(({ id }) => {
+      dispatch(mopidyActions.getServerState(id));
+    });
+  }, []);
+
+  return (
+    <>
+      {map(groupsByStream, (groups, stream_id) => {
+        const clients_ids = groups.reduce((acc, curr) => [...acc, ...curr.clients_ids], []);
+        const group = {
+          stream: find(streams, (s) => s.id === stream_id),
+          server: find(servers, (s) => s.snapcast_stream === stream_id),
+          clients: clients_ids.map((id) => clients[id]).filter((c) => c.connected),
+        };
+        return <Group item={group} key={stream_id} />;
+      })}
+    </>
+  );
+}
 
 const SnapcastGroups = () => {
   const dispatch = useDispatch();
@@ -91,7 +242,7 @@ const Commands = () => {
   items = sortItems(items, 'sort_order');
 
   return (
-    <div className="output-control__item output-control__item--commands commands">
+    <div className="output-control__commands commands">
       {
         items.map((command) => (
           <div
@@ -106,28 +257,7 @@ const Commands = () => {
       }
     </div>
   );
-}
-
-const Outputs = () => {
-  const snapcastGroups = <SnapcastGroups />;
-  const commands = <Commands />;
-
-  if (!snapcastGroups && !commands) {
-    return (
-      <div className="output-control__items output-control__items--no-results">
-        <p className="no-results">
-          <I18n path="playback_controls.no_outputs" />
-        </p>
-      </div>
-    );
-  }
-  return (
-    <div className="output-control__items">
-      {commands}
-      {snapcastGroups}
-    </div>
-  );
-}
+};
 
 const OutputControl = ({
   force_expanded,
@@ -155,7 +285,20 @@ const OutputControl = ({
     }
   }, [expanded]);
 
+  // No customisable outputs
+  if (!snapcast_enabled && !pusher_commands) {
+    return (
+      <span className="output-control disabled">
+        <button className="control speakers">
+          <Icon name="speaker" />
+        </button>
+      </span>
+    );
+  }
+
   if (expanded) {
+    const outputs = <Outputs />;
+    const commands = <Commands />;
     return (
       <span className="output-control">
         <button
@@ -164,18 +307,18 @@ const OutputControl = ({
         >
           <Icon name="speaker" />
         </button>
-        <Outputs />
-      </span>
-    );
-  }
-
-  // No customisable outputs
-  if (!snapcast_enabled && !pusher_commands) {
-    return (
-      <span className="output-control disabled">
-        <button className="control speakers">
-          <Icon name="speaker" />
-        </button>
+        {!outputs && !commands ? (
+          <div className="output-control__inner output-control__inner--no-results">
+            <p className="no-results">
+              <I18n path="playback_controls.no_outputs" />
+            </p>
+          </div>
+        ) : (
+          <div className="output-control__inner">
+            {commands}
+            {outputs}
+          </div>
+        )}
       </span>
     );
   }
