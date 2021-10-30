78a807e2c8aa3a2fe37ae2d8d10b76d00f287fa4
Bas van Boven <bas@basvanboven.nl>
2021-10-30 14:06:24 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,8 +7,8 @@ then
 fi
 
 if [ ${PIP_PACKAGES:+x} ]; then
-	echo "-- INSTALLING PIP PACKAGES $PIP_PACKAGES --"
-	python3 -m pip install --no-cache $PIP_PACKAGES
+    echo "-- INSTALLING PIP PACKAGES $PIP_PACKAGES --"
+    python3 -m pip install --no-cache $PIP_PACKAGES
 fi
 
 exec "$@"
