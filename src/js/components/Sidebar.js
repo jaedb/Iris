cfca0b95d437f0f279663a9894c043c07f0df9bc
Joel Takvorian <jtakvori@redhat.com>
2022-08-07 15:49:57 +0200

    Distinguish "missing spotify token" vs "spotify not enabled"

--- src/js/components/Sidebar.js
+++ src/js/components/Sidebar.js
@@ -66,7 +66,8 @@ const StatusIcon = () => {
 
 const Sidebar = () => {
   const dispatch = useDispatch();
-  const spotify_available = useSelector((state) => state.spotify.access_token);
+  const spotify_enabled = useSelector((state) => state.spotify.enabled);
+  const spotify_has_token = useSelector((state) => state.spotify.access_token);
 
   const close = () => dispatch(toggleSidebar(false));
 
@@ -85,27 +86,31 @@ const Sidebar = () => {
             </Link>
           </section>
 
-          {spotify_available && (
+          {spotify_enabled && (
             <section className="sidebar__menu__section">
               <title className="sidebar__menu__section__title">
                 <I18n path="sidebar.discover" />
               </title>
-              <Link
-                to="/discover/recommendations"
-                className="sidebar__menu__item"
-                activeClassName="sidebar__menu__item--active"
-              >
-                <Icon name="explore" type="material" />
-                <I18n path="sidebar.discover" />
-              </Link>
+              {spotify_has_token && (
+                <Link
+                  to="/discover/recommendations"
+                  className="sidebar__menu__item"
+                  activeClassName="sidebar__menu__item--active"
+                >
+                  <Icon name="explore" type="material" />
+                  <I18n path="sidebar.discover" />
+                </Link>
+              )}
               <Link to="/discover/moods" className="sidebar__menu__item" activeClassName="sidebar__menu__item--active">
                 <Icon name="mood" type="material" />
                 <I18n path="sidebar.moods" />
               </Link>
-              <Link to="/discover/featured-playlists" className="sidebar__menu__item" activeClassName="sidebar__menu__item--active">
-                <Icon name="star" type="material" />
-                <I18n path="sidebar.featured_playlists" />
-              </Link>
+              {spotify_has_token && (
+                <Link to="/discover/featured-playlists" className="sidebar__menu__item" activeClassName="sidebar__menu__item--active">
+                  <Icon name="star" type="material" />
+                  <I18n path="sidebar.featured_playlists" />
+                </Link>
+              )}
               <Link to="/discover/new-releases" className="sidebar__menu__item" activeClassName="sidebar__menu__item--active">
                 <Icon name="new_releases" type="material" />
                 <I18n path="sidebar.new_releases" />
